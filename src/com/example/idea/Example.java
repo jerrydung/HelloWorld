9b9315768843b61f9a3e5b65b21c74306bcd074d
Ming T.D <mdung@guidewire.com>
2020-08-25 22:09:26 -0700

    initial commit

---
+++ src/com/example/idea/Example.java
@@ -0,0 +1,36 @@
+package com.example.idea;
+
+/**
+ * This is a testing class
+ */
+public class Example {
+
+    private String secrect;
+
+    public Example(String secrect) {
+        this.secrect =secrect;
+
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Example test = new Example("Hello!!");
+        test.printSecrect();
+        String outSecrect = test.getSecrect();
+        outSecrect = "Modified "+outSecrect;
+        test.printSecrect();
+
+    }
+
+    public String getSecrect() {
+        return secrect;
+    }
+
+    public void printSecrect() {
+        System.out.println("--> "+secrect);
+
+    }
+
+}
+
