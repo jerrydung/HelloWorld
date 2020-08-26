9da2f712ec7a5caf3cab9bcb6194bc14a5cbf6cf
Ming T.D <mdung@guidewire.com>
2020-08-26 00:14:18 -0700

    output arguments

--- src/com/example/idea/Main.java
+++ src/com/example/idea/Main.java
@@ -3,7 +3,7 @@ package com.example.idea;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello from IntelliJ IDEA!");
+
+        System.out.println("Hello from IntelliJ IDEA! "+args);
     }
 }
